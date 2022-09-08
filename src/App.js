d4faf330aec2ab6221f3c153ab18442633a45373
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-08 16:38:14 +0600

    useTimeout hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import ToggleComponent from "./1.useToggle/toggleComponent";
+import TimeoutComponent from "./2.useTimeout/timeoutComponent";
 
 function App() {
-  return (
-    <>
-      <ToggleComponent />
-    </>
-  );
+  // return <ToggleComponent />;
+  return <TimeoutComponent />;
 }
 
 export default App;
