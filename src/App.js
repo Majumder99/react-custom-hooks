aa398ec6e2be6adfb88d050b1a09f074c4eb1624
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-08 16:48:22 +0600

    useDebounce hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ToggleComponent from "./1.useToggle/toggleComponent";
 import TimeoutComponent from "./2.useTimeout/timeoutComponent";
+import DebounceComponent from "./3.useDebounce/debounceComponent";
 
 function App() {
   // return <ToggleComponent />;
-  return <TimeoutComponent />;
+  // return <TimeoutComponent />;
+  return <DebounceComponent />;
 }
 
 export default App;
