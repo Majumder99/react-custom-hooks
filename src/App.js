e8bc3a77a6a3898e7a1b96daf4773f42ab357eff
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-08 21:04:12 +0600

    5 custom hooks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import ToggleComponent from "./1.useToggle/toggleComponent";
 import TimeoutComponent from "./2.useTimeout/timeoutComponent";
 import DebounceComponent from "./3.useDebounce/debounceComponent";
 import UpdateEffectComponent from "./4.useUpdateEffect/updateEffectComponent";
+import ArrayComponent from "./5.useArray/ArrayComponent";
 
 function App() {
   // return <ToggleComponent />;
   // return <TimeoutComponent />;
   // return <DebounceComponent />;
-  return <UpdateEffectComponent />;
+  // return <UpdateEffectComponent />;
+  return <ArrayComponent />;
 }
 
 export default App;
